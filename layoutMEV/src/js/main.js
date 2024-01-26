a210cfc0db94a7e4d16d2e6c3e9a090e060b062c
María Escribano Verde <maria.escribanoverde@gmail.com>
2024-01-26 19:56:20 +0100

    Toast

--- layoutMEV/src/js/main.js
+++ layoutMEV/src/js/main.js
@@ -4,27 +4,19 @@ import '../scss/main.scss';
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap';
 
-/** 
-//no permite seleccionar fechas futuras ni fechas anteriores a una cierta edad (18 años atrás desde la fecha actual)
-// Obtén la fecha actual
-const fechaActual = new Date();
-
-// Calcula la fecha mínima (hace 18 años a partir de la fecha actual)
-const fechaMinima = new Date(fechaActual);
-fechaMinima.setFullYear(fechaMinima.getFullYear() - 18);
-
-// Formatea la fecha mínima en el formato necesario para el campo de fecha
-const dia = fechaMinima.getDate().toString().padStart(2, '0');
-const mes = (fechaMinima.getMonth() + 1).toString().padStart(2, '0'); // Suma 1 porque enero es 0
-const anio = fechaMinima.getFullYear();
-
-const fechaMinimaFormateada = `${anio}-${mes}-${dia}`;
-
-const fechaNacimientoInput = document.getElementById("fecha-nacimiento");
-fechaNacimientoInput.max = fechaMinimaFormateada;
-*/
-
 //Toast
+document.addEventListener('DOMContentLoaded', function () {
+    var toastElement = document.getElementById('liveToast');
+    var myToast = toastElement ? new bootstrap.Toast(toastElement) : null;
+
+    var toastBtn = document.getElementById('liveToastBtn');
+    if (myToast && toastBtn) {
+        toastBtn.addEventListener('click', function () {
+            myToast.show();
+        });
+    }
+});
+
 
 //Tooltips
 var tooltips = new bootstrap.Tooltip(document.body, {
@@ -44,3 +36,23 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
+
+/** 
+//no permite seleccionar fechas futuras ni fechas anteriores a una cierta edad (18 años atrás desde la fecha actual)
+// Obtén la fecha actual
+const fechaActual = new Date();
+
+// Calcula la fecha mínima (hace 18 años a partir de la fecha actual)
+const fechaMinima = new Date(fechaActual);
+fechaMinima.setFullYear(fechaMinima.getFullYear() - 18);
+
+// Formatea la fecha mínima en el formato necesario para el campo de fecha
+const dia = fechaMinima.getDate().toString().padStart(2, '0');
+const mes = (fechaMinima.getMonth() + 1).toString().padStart(2, '0'); // Suma 1 porque enero es 0
+const anio = fechaMinima.getFullYear();
+
+const fechaMinimaFormateada = `${anio}-${mes}-${dia}`;
+
+const fechaNacimientoInput = document.getElementById("fecha-nacimiento");
+fechaNacimientoInput.max = fechaMinimaFormateada;
+*/
